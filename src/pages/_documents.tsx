6dd294d01400c6a3682cda575fc21f10ed5ad0d5
thiagodemas7 <thiago.demas@ignicaodigital.com.br>
2022-04-27 09:29:51 -0300

    Modifica a font utilizada pelo chakra-ui

--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -6,8 +6,7 @@ export default class MyDocuments extends Documents {
             <Html>
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet"></link>
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
                 </Head>
                 <body>
                     <Main />
