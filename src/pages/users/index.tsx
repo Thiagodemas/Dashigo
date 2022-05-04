686da2b8c60666c430054319631ebe4092db55e1
thiagodemas7 <thiago.demas@ignicaodigital.com.br>
2022-05-04 09:26:32 -0300

    Cria página de adicionar usuários

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,113 +5,113 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
-    return (
-        <Box>
-            <Header />
-            <Flex
-                w="100%"
-                my="6"
-                maxWidth={1480}
-                mx="auto"
-                px="6"
+  return (
+    <Box>
+      <Header />
+      <Flex
+        w="100%"
+        my="6"
+        maxWidth={1480}
+        mx="auto"
+        px="6"
+      >
+        <Sidebar />
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
             >
-                <Sidebar />
-                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
-                    <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+              Criar novo
+            </Button>
+          </Flex>
 
-                        <Button
-                            as="a"
-                            size="sm"
-                            fontSize="sm"
-                            colorScheme="pink"
-                            leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-                        >
-                            Criar novo
-                        </Button>
-                    </Flex>
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuários</Th>
+                <Th>Data de Cadastro</Th>
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr px="6">
+                <Td>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Thiago Demas</Text>
+                    <Text fontSize="sm" color="gray.300">thiago.demas7@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td> 19 de Maio, 2022</Td>
+                <Td> <Button
+                  as="a"
+                  size="sm"
+                  fontSize="sm"
+                  colorScheme="purple"
+                  leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                >
+                  Editar
+                </Button></Td>
+              </Tr>
+              <Tr px="6">
+                <Td>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Thiago Demas</Text>
+                    <Text fontSize="sm" color="gray.300">thiago.demas7@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td> 19 de Maio, 2022</Td>
+                <Td> <Button
+                  as="a"
+                  size="sm"
+                  fontSize="sm"
+                  colorScheme="purple"
+                  leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                >
+                  Editar
+                </Button></Td>
+              </Tr>
+              <Tr px="6">
+                <Td>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Thiago Demas</Text>
+                    <Text fontSize="sm" color="gray.300">thiago.demas7@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td> 19 de Maio, 2022</Td>
+                <Td> <Button
+                  as="a"
+                  size="sm"
+                  fontSize="sm"
+                  colorScheme="purple"
+                  leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                >
+                  Editar
+                </Button></Td>
+              </Tr>
+            </Tbody>
+          </Table>
 
-                    <Table colorScheme="whiteAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th px="6" color="gray.300" width="8">
-                                    <Checkbox colorScheme="pink" />
-                                </Th>
-                                <Th>Usuários</Th>
-                                <Th>Data de Cadastro</Th>
-                                <Th w="8"></Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr px="6">
-                                <Td>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Thiago Demas</Text>
-                                        <Text fontSize="sm" color="gray.300">thiago.demas7@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                <Td> 19 de Maio, 2022</Td>
-                                <Td> <Button
-                                    as="a"
-                                    size="sm"
-                                    fontSize="sm"
-                                    colorScheme="purple"
-                                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                >
-                                    Editar
-                                </Button></Td>
-                            </Tr>
-                            <Tr px="6">
-                                <Td>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Thiago Demas</Text>
-                                        <Text fontSize="sm" color="gray.300">thiago.demas7@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                <Td> 19 de Maio, 2022</Td>
-                                <Td> <Button
-                                    as="a"
-                                    size="sm"
-                                    fontSize="sm"
-                                    colorScheme="purple"
-                                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                >
-                                    Editar
-                                </Button></Td>
-                            </Tr>
-                            <Tr px="6">
-                                <Td>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Thiago Demas</Text>
-                                        <Text fontSize="sm" color="gray.300">thiago.demas7@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                <Td> 19 de Maio, 2022</Td>
-                                <Td> <Button
-                                    as="a"
-                                    size="sm"
-                                    fontSize="sm"
-                                    colorScheme="purple"
-                                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                >
-                                    Editar
-                                </Button></Td>
-                            </Tr>
-                        </Tbody>
-                    </Table>
-
-                    <Pagination />
-                </Box>
-            </Flex>
+          <Pagination />
         </Box>
-    );
+      </Flex>
+    </Box>
+  );
 }
